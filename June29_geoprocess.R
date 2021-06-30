a009d85095ba113759b2ebb589936f801f151ece
shannon-dolan <smdolan@ncsu.edu>
2021-06-30 11:59:09 -0700

    Updated code.

--- June29_geoprocess.R
+++ June29_geoprocess.R
@@ -6,8 +6,8 @@ library(ggplot2)
 library(sp)
 library(dplyr)
 
-nasc.spatialp <- sf::st_as_sf(nasc, coords=c("long", "lat"))
-bound.box <- (sf::st_bbox(nasc.spatialp))
+nasc.sf <- sf::st_as_sf(nasc, coords=c("long", "lat"))
+bound.box <- (sf::st_bbox(nasc.sf))
 print(bound.box)
 box.old <- st_as_sfc(st_bbox(bound.box))
 
@@ -23,7 +23,7 @@ box <- data.frame(x= as.numeric(c("-129.25383", "-117.27371", "-117.27371","-129
 
 
 #remove clusters with zero catch 
-cluster <- cluster.summ.wt[which(!cluster.summ.wt$cluster %in% cluster.zero$cluster),]
+cluster <- as.data.frame(cluster.summ.wt[which(!cluster.summ.wt$cluster %in% cluster.zero$cluster),])
 
 ##plot trawl clusters, transects, and voronoi tessellation 
 
@@ -37,4 +37,33 @@ ggplot()+
              aes(long, lat))+
   geom_text(data=cluster, 
             aes(long, lat, label=cluster), size=3, hjust=-1, vjust=0)
+
+
+## convert to spdf  
+
+cluster.spdf <- voronoi_polygon(cluster, x="long", y="lat", outline=box)
+cluster.df <- fortify_voronoi(cluster.spdf)
+
+
+#plot spdf in ggplot 
+
+ggplot()+coord_map()+theme_bw()+
+  geom_path(data = nasc, aes(long, lat))+
+  geom_polygon(data=cluster.spdf,
+               aes(x=long, y=lat, fill=group, group=group),color="black", alpha=0.8)+
+  geom_point(data=cluster, 
+             aes(long, lat))+
+  geom_text(data=cluster, 
+            aes(long, lat, label=cluster), size=3, hjust=-1, vjust=0)
+
+mapview::mapview(cluster.spdf)
+
+#convert to sf
+
+cluster.sf <- as(cluster.spdf, "sf")
+class(cluster.sf)
+
+#maybe join? idk I'm working on it now
+
+cluster.nasc.join <- st_join(cluster.sf, nasc.sf, join=st_intersects)
 
